e1135e9f73c61454c9a39e21dc48aa0d307664d0
Cotter <Cotter@donovandlabsmacbookpro.local>
2016-09-09 10:32:39 -0400

    Initial Commit

---
+++ MFCUTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MFCUTest
+//
+//  Created by Cotter on 9/9/16.
+//  Copyright © 2016 Cotter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
